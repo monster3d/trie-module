abc07a65eee64e43bbae06de78b54fdc734b4b0b
Nikolay Baev <monster3d@mail.ru>
2019-04-03 12:48:16 +0500

    Added example hardcode main enter point

---
+++ main.c
@@ -0,0 +1,23 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "inc.c"
+
+
+int main() {
+    struct trie *root;
+
+    root = trie_insert(NULL, "bars", "60");
+    root = trie_insert(root, "baribal", "100");
+    root = trie_insert(root, "kit", "3000");
+    root = trie_insert(root, "lev", "500");
+    root = trie_insert(root, "bars", "70");
+    trie_print(root, 0);
+
+    printf("Lookup 'bars': %s\n", trie_lookup(root, "bars"));
+    root = trie_delete(root, "bars");
+    trie_print(root, 0);
+   
+    return 0;
+}
